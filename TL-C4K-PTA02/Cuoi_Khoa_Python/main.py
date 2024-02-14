e2b86febe6a9c0273c1c67b24df5adc2ce8bc7a1
DinhPhongNe <phongvo2405@example.com>
2024-02-14 15:53:24 +0700

    update

--- TL-C4K-PTA02/Cuoi_Khoa_Python/main.py
+++ TL-C4K-PTA02/Cuoi_Khoa_Python/main.py
@@ -1,17 +1,40 @@
 import sys
 from PyQt6.QtCore import Qt
-from PyQt6.QtWidgets import QMainWindow, QApplication, QWidget
+from PyQt6.QtWidgets import QMainWindow, QApplication, QWidget, QMessageBox
 from PyQt6 import uic
 
 
 class Main(QMainWindow):
     def __init__(self) -> None:
         super().__init__()
-        uic.loadUi("menu.ui", self)
+        uic.loadUi("gui/menu.ui", self)
+        
+        self.HocSinh_btn.clicked.connect(self.Login_hs)
+        self.GiaoVien_btn.clicked.connect(self.Login_tc)
+        
+    def Login_hs(self):
+        uic.loadUi("gui/login-student.ui", self)
+            
+    def Login_tc(self):
+        uic.loadUi("gui/login-teacher.ui", self)
+        
+
+class Main_ST(QMainWindow):
+    pass
+
+class Main_TC(QMainWindow):
+    pass
 
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     window = Main()
-    window.show()
+    st_page = Main_ST()
+    tc_page = Main_TC()
+    
+    #thông báo lỗi
+    msg_box = QMessageBox()
+    msg_box.setWindowTitle("Lỗi")
+    msg_box.setIcon(QMessageBox.Icon.Warning)
+    msg_box.setStyleSheet("background-color: #F8F2EC; color: #356a9c")
     sys.exit(app.exec())
