ded8afe1375d59d56f8e946909e04ab1180ec040
DinhPhongNe <phongvo2405@example.com>
2024-02-24 10:12:49 +0700

    update

--- TL-C4K-PTA02/Cuoi_Khoa_Python/main.py
+++ TL-C4K-PTA02/Cuoi_Khoa_Python/main.py
@@ -11,6 +11,7 @@ class Main(QMainWindow):
         
         self.HocSinh_btn.clicked.connect(self.Login_hs)
         self.GiaoVien_btn.clicked.connect(self.Login_tc)
+        self.Register_btn.clicked.connect(self.regis)
         
         
     #==--Phần của giáo viên--==
@@ -18,9 +19,15 @@ class Main(QMainWindow):
     def Login_tc(self):
         uic.loadUi("gui/login-teacher.ui", self)
         self.GiaoVienLogin_btn.clicked.connect(self.GiaoVienClicked)
+        self.goback_tc_btn.clicked.connect(self.goback_tc_Clicked)
 
     def GiaoVienClicked(self):
         uic.loadUi("gui/main-tc.ui", self)
+        
+    def goback_tc_Clicked(self):
+        uic.loadUi("gui/menu.ui",self)
+        
+
     
     
     
@@ -29,9 +36,26 @@ class Main(QMainWindow):
     def Login_hs(self):
         uic.loadUi("gui/login-student.ui", self)
         self.HocSinhLogin_btn.clicked.connect(self.HocSinhClicked) 
+        self.goback_hs_btn.clicked.connect(self.goback_hs_Clicked)
         
     def HocSinhClicked(self):
         uic.loadUi("gui/main-st.ui", self)
+        
+    def goback_hs_Clicked(self):
+        uic.loadUi("gui/menu.ui",self)
+        
+        
+        
+        
+    #==--Phần của Đăng ký--== 
+    def regis(self):
+        uic.loadUi("gui/register.ui", self)
+        self.register_btn.clicked.connect(self.menu_return)
+        
+    def menu_return(self):
+        uic.loadUi("gui/menu.ui",self)
+    
+    
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
