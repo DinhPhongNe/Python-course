c94a440febd821410665864c1895a534aef96b2e
DinhPhongNe <phongvo2405@gmail.com>
2024-02-25 09:40:33 +0700

    update

--- TL-C4K-PTA02/Cuoi_Khoa_Python/main.py
+++ TL-C4K-PTA02/Cuoi_Khoa_Python/main.py
@@ -1,6 +1,6 @@
 import sys
 from PyQt6.QtCore import Qt
-from PyQt6.QtWidgets import QMainWindow, QApplication, QWidget, QMessageBox, QPushButton
+from PyQt6.QtWidgets import  QMainWindow, QApplication, QWidget, QMessageBox, QPushButton
 from PyQt6 import uic
 
 
@@ -13,52 +13,92 @@ class Main(QMainWindow):
         self.GiaoVien_btn.clicked.connect(self.Login_tc)
         self.Register_btn.clicked.connect(self.regis)
         
+        # Lưu trữ các giao diện con
+        self.teacher_login = None
+        self.student_login = None
+        self.register = None
+        self.teacher_main = None
+        self.student_main = None
+        self.renewpass = None
         
-    #==--Phần của giáo viên--==
-    #Login của giáo viên
+        
+        
+        
+    # Phần của giáo viên
     def Login_tc(self):
-        uic.loadUi("gui/login-teacher.ui", self)
-        self.GiaoVienLogin_btn.clicked.connect(self.GiaoVienClicked)
-        self.goback_tc_btn.clicked.connect(self.goback_tc_Clicked)
+        if not self.teacher_login:
+            self.teacher_login = uic.loadUi("gui/login-teacher.ui")
+            self.teacher_login.GiaoVienLogin_btn.clicked.connect(self.GiaoVienClicked)
+            self.teacher_login.goback_tc_btn.clicked.connect(self.goback_tc_Clicked)
+            self.teacher_login.forgo_pass_tc.clicked.connect(self.renew)
+
+        self.teacher_login.show()
+        self.hide()
 
     def GiaoVienClicked(self):
-        uic.loadUi("gui/main-tc.ui", self)
+        if not self.teacher_main:
+            self.teacher_main = uic.loadUi("gui/main-tc.ui")
+        
+        self.teacher_main.show()
+        self.hide()
         
     def goback_tc_Clicked(self):
-        uic.loadUi("gui/menu.ui",self)
-        
+        self.teacher_login.hide()
+        self.show()
 
-    
-    
-    
-    #==--Phần của học sinh--== 
-    #Login của học sinh
+
+
+
+    # Phần của học sinh
     def Login_hs(self):
-        uic.loadUi("gui/login-student.ui", self)
-        self.HocSinhLogin_btn.clicked.connect(self.HocSinhClicked) 
-        self.goback_hs_btn.clicked.connect(self.goback_hs_Clicked)
-        
+        if not self.student_login:
+            self.student_login = uic.loadUi("gui/login-student.ui")
+            self.student_login.HocSinhLogin_btn.clicked.connect(self.HocSinhClicked)
+            self.student_login.goback_hs_btn.clicked.connect(self.goback_hs_Clicked)
+            self.student_login.forgo_pass_hs.clicked.connect(self.renew)
+
+        self.student_login.show()
+        self.hide()
+
     def HocSinhClicked(self):
-        uic.loadUi("gui/main-st.ui", self)
+        if not self.student_main:
+            self.student_main = uic.loadUi("gui/main-st.ui")
+        
+        self.student_main.show()
+        self.hide()
         
     def goback_hs_Clicked(self):
-        uic.loadUi("gui/menu.ui",self)
-        
-        
-        
-        
-    #==--Phần của Đăng ký--== 
+        self.student_login.hide()
+        self.show()
+
+
+    def renew(self):
+        if not self.renewpass:
+            self.renewpass = uic.loadUi("gui/renewpass.ui")
+            
+        self.renewpass.show()
+        self.hide
+
+    # Phần đăng ký
     def regis(self):
-        uic.loadUi("gui/register.ui", self)
-        self.register_btn.clicked.connect(self.menu_return)
+        if not self.register:
+            self.register = uic.loadUi("gui/register.ui")
+            self.register.register_btn.clicked.connect(self.menu_return)
+            self.register.goback_reg.clicked.connect(self.menu_return)
+        
+        self.register.show()
+        self.hide()
+        
+    
+        
+        
         
     def menu_return(self):
-        uic.loadUi("gui/menu.ui",self)
-    
-    
+        self.register.hide()
+        self.show()
 
 if __name__ == "__main__":
-    app = QApplication(sys.argv)
+    app = QApplication([])
     window = Main()
     window.show()
-    sys.exit(app.exec())
+    app.exec()
